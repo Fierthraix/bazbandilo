eeb5e3d45356807811d7e8e7549f35d0994c8759
Lucas Rooyakkers <lucas.rooyakkers@pm.me>
2024-09-15 22:04:39 -0400

    Fixed CSK and DCSK to be baseband.

--- src/csk.rs
+++ src/csk.rs
@@ -2,9 +2,7 @@ use crate::{chaos::LogisticMap, Bit};
 
 use num_complex::Complex;
 
-pub fn tx_baseband_csk_signal<I: Iterator<Item = Bit>>(
-    message: I,
-) -> impl Iterator<Item = Complex<f64>> {
+pub fn tx_csk_signal<I: Iterator<Item = Bit>>(message: I) -> impl Iterator<Item = Complex<f64>> {
     let mu = 3.9;
     let x0_1 = 0.1;
     let x0_2 = 0.15;
@@ -14,9 +12,7 @@ pub fn tx_baseband_csk_signal<I: Iterator<Item = Bit>>(
         .map(|((bit, chaos_1), chaos_2)| Complex::new(if bit { chaos_1 } else { chaos_2 }, 0f64))
 }
 
-pub fn rx_baseband_csk_signal<I: Iterator<Item = Complex<f64>>>(
-    message: I,
-) -> impl Iterator<Item = Bit> {
+pub fn rx_csk_signal<I: Iterator<Item = Complex<f64>>>(message: I) -> impl Iterator<Item = Bit> {
     let mu = 3.9;
     let x0_1 = 0.1;
     let x0_2 = 0.15;
@@ -34,13 +30,13 @@ mod tests {
     use crate::Rng;
 
     #[test]
-    fn baseband_csk() {
+    fn csk() {
         let mut rng = rand::thread_rng();
         let num_bits = 9001;
         let data_bits: Vec<Bit> = (0..num_bits).map(|_| rng.gen::<Bit>()).collect();
 
-        let csk_tx: Vec<Complex<f64>> = tx_baseband_csk_signal(data_bits.iter().cloned()).collect();
-        let csk_rx: Vec<Bit> = rx_baseband_csk_signal(csk_tx.iter().cloned()).collect();
+        let csk_tx: Vec<Complex<f64>> = tx_csk_signal(data_bits.iter().cloned()).collect();
+        let csk_rx: Vec<Bit> = rx_csk_signal(csk_tx.iter().cloned()).collect();
         assert_eq!(data_bits, csk_rx);
     }
 }
--- src/dcsk.rs
+++ src/dcsk.rs
@@ -1,16 +1,21 @@
 use crate::{bit_to_nrz, chaos::LogisticMap, Bit};
 use itertools::Itertools;
+use num_complex::Complex;
 
-pub fn tx_baseband_dcsk<I: Iterator<Item = Bit>>(message: I) -> impl Iterator<Item = f64> {
+pub fn tx_dcsk_signal<I: Iterator<Item = Bit>>(message: I) -> impl Iterator<Item = Complex<f64>> {
     message
         .zip(LogisticMap::new(3.9, 0.1))
-        .flat_map(|(bit, reference)| [reference, reference * bit_to_nrz(bit)].into_iter())
+        .flat_map(|(bit, reference)| {
+            [reference, reference * bit_to_nrz(bit)]
+                .into_iter()
+                .map(|s_i| Complex::new(s_i, 0f64))
+        })
 }
 
-pub fn rx_baseband_dcsk<I: Iterator<Item = f64>>(message: I) -> impl Iterator<Item = Bit> {
+pub fn rx_dcsk_signal<I: Iterator<Item = Complex<f64>>>(message: I) -> impl Iterator<Item = Bit> {
     message
         .tuples()
-        .map(|(reference, information)| reference * information > 0f64)
+        .map(|(reference, information)| reference.re * information.re > 0f64)
 }
 
 #[cfg(test)]
@@ -21,13 +26,13 @@ mod tests {
     use crate::dcsk::tests::rand::Rng;
 
     #[test]
-    fn baseband_dcsk() {
+    fn dcsk_signal() {
         let mut rng = rand::thread_rng();
         let num_bits = 9001;
         let data_bits: Vec<Bit> = (0..num_bits).map(|_| rng.gen::<Bit>()).collect();
 
-        let dcsk_tx: Vec<f64> = tx_baseband_dcsk(data_bits.iter().cloned()).collect();
-        let dcsk_rx: Vec<Bit> = rx_baseband_dcsk(dcsk_tx.iter().cloned()).collect();
+        let dcsk_tx: Vec<Complex<f64>> = tx_dcsk_signal(data_bits.iter().cloned()).collect();
+        let dcsk_rx: Vec<Bit> = rx_dcsk_signal(dcsk_tx.iter().cloned()).collect();
         assert_eq!(data_bits, dcsk_rx);
     }
 }
