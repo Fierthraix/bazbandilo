a9957547c81bc8ecbe327170939e9c1a1ae614fc
Lucas Rooyakkers <lucas.rooyakkers2@ecn.forces.gc.ca>
2026-06-09 21:39:55 -0400

    Update graph copy destination

--- final/graphs/cp.sh
+++ final/graphs/cp.sh
@@ -5,8 +5,10 @@ ROOT="$(git rev-parse --show-toplevel)"
 set -ex
 
 DATA_DIR="${ROOT}/final"
-GRAPHS_DIR="${DATA_DIR}/graphs"
-FIG_DEST_DIR="${HOME}/dosieroj/rmc/tezo/papero/img/fig"
+# GRAPHS_DIR="${DATA_DIR}/graphs"
+GRAPHS_DIR="${DATA_DIR}/paper_graphs/paper_graphs"
+# FIG_DEST_DIR="${HOME}/dosieroj/rmc/tezo/papero/img/fig"
+FIG_DEST_DIR="${HOME}/papers/papero/img/fig"
 
 
 rsync -avx "${GRAPHS_DIR}"/ "${FIG_DEST_DIR}"
